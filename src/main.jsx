298d31a8a3796e70d1e60f8713f3d0ab4d0449c1
railwaytohell <kenzaburooe.luc@gmail.com>
2025-10-17 12:37:19 +0300

    workflows addsd

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App.jsx'
-import Home from './pages/Home.jsx'
-import Catalog from './pages/Catalog.jsx'
-
-const router = createBrowserRouter([
+-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
++import { createHashRouter, RouterProvider } from 'react-router-dom'
+...
+-const router = createBrowserRouter([
++const router = createHashRouter([
   { path: '/', element: <App />, children: [
     { index: true, element: <Home /> },
     { path: 'catalog', element: <Catalog /> },
   ]},
 ])
-
-createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
